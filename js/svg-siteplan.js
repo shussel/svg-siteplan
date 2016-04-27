d084682425d92ecfe9213398a1b1c98860063075
Shane Hussel <shussel@gmail.com>
2016-04-27 18:57:40 -0400

    label spaces with javscript

--- js/svg-siteplan.js
+++ js/svg-siteplan.js
@@ -5,6 +5,7 @@ request.open('GET', '/siteplan.json', true);
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
     var data = JSON.parse(request.responseText);
+	init(data);
   } else {
     console.log('file not found');
   }
@@ -15,4 +16,37 @@ request.onerror = function() {
 };
 
 request.send();
+
+function init(data) {
+
+	var spaces = Object.keys(data);
+
+	spaces.forEach(function(space) {
+
+		if(el = document.getElementById(space)) {
+		
+			var bbox = el.getBBox(); 
+			var x = Math.floor(bbox.x + bbox.width/2.0);
+			var y = Math.floor(bbox.y + bbox.height/2.0);
+
+			var newText = document.createElementNS("http://www.w3.org/2000/svg","text");
+			newText.setAttributeNS(null,"x",x);     
+			newText.setAttributeNS(null,"y",y); 
+			newText.setAttributeNS(null,"font-size","10");
+			newText.setAttributeNS(null,"font-family","Arial");
+			newText.setAttributeNS(null,"font-weight","bold");
+			newText.setAttributeNS(null,"text-anchor","middle");
+			newText.setAttributeNS(null,"dominant-baseline", "central");			
+
+			var textNode = document.createTextNode(data[space]["label"]);
+			newText.appendChild(textNode);
+			document.getElementById("Text").appendChild(newText);
+
+			if (data[space]["available"] == true)
+			{			
+				el.setAttribute('fill', "#ff0000");
+			}
+		}
+	});
+}
 
