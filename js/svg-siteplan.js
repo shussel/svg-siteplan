a359560158861db5dae21b7389a070c77508a66f
Shane Hussel <shussel@gmail.com>
2016-04-27 22:12:34 -0400

    add javascript hovering

--- js/svg-siteplan.js
+++ js/svg-siteplan.js
@@ -27,7 +27,7 @@ function init(data) {
 		
 			var bbox = el.getBBox(); 
 			var x = Math.floor(bbox.x + bbox.width/2.0);
-			var y = Math.floor(bbox.y + bbox.height/2.0);
+			var y = Math.floor(bbox.y + bbox.height/2.0) + 5;
 
 			var newText = document.createElementNS("http://www.w3.org/2000/svg","text");
 			newText.setAttributeNS(null,"x",x);     
@@ -35,8 +35,7 @@ function init(data) {
 			newText.setAttributeNS(null,"font-size","10");
 			newText.setAttributeNS(null,"font-family","Arial");
 			newText.setAttributeNS(null,"font-weight","bold");
-			newText.setAttributeNS(null,"text-anchor","middle");
-			newText.setAttributeNS(null,"dominant-baseline", "central");			
+			newText.setAttributeNS(null,"text-anchor","middle");			
 
 			var textNode = document.createTextNode(data[space]["label"]);
 			newText.appendChild(textNode);
@@ -44,9 +43,33 @@ function init(data) {
 
 			if (data[space]["available"] == true)
 			{			
-				el.setAttribute('fill', "#ff0000");
+				el.setAttribute('fill', "#fefda0");
+				el.addEventListener("mouseover", hoverAvailable);
+				el.addEventListener("mouseout", dehoverAvailable);
+			} else {
+				el.addEventListener("mouseover", hoverOccupied);
+				el.addEventListener("mouseout", dehoverOccupied);
 			}
 		}
 	});
 }
 
+function hoverAvailable(e) {
+	this.setAttribute('fill', "#ff0000");
+	document.getElementById("avail").setAttribute('fill', "#ff0000");
+}
+
+function dehoverAvailable(e) {
+	this.setAttribute('fill', "#fefda0");
+	document.getElementById("avail").setAttribute('fill', "#fefda0");
+}
+
+function hoverOccupied(e) {
+	this.setAttribute('fill', "#add8e6");
+	document.getElementById("occ").setAttribute('fill', "#add8e6");
+}
+
+function dehoverOccupied(e) {
+	this.setAttribute('fill', "#eeeeee");
+	document.getElementById("occ").setAttribute('fill', "#eeeeee");
+}
